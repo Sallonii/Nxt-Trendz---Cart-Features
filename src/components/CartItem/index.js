47ae07409b5bf2ba0e18bd48fb389bdabad7f107
Sallonii <121022481+Sallonii@users.noreply.github.com>
2024-05-25 16:53:27 +0530

    Update index.js

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -1,87 +1,47 @@
-import {BsPlusSquare, BsDashSquare} from 'react-icons/bs'
-import {AiFillCloseCircle} from 'react-icons/ai'
+import Header from '../Header'
+import CartListView from '../CartListView'
 
 import CartContext from '../../context/CartContext'
+import EmptyCartView from '../EmptyCartView'
+import CartSummary from '../CartSummary'
 
 import './index.css'
 
-const CartItem = props => (
+const Cart = () => (
   <CartContext.Consumer>
     {value => {
-      const {
-        removeCartItem,
-        incrementCartItemQuantity,
-        decrementCartItemQuantity,
-      } = value
-      const {cartItemDetails} = props
-      const {id, title, brand, quantity, price, imageUrl} = cartItemDetails
-      const onRemoveCartItem = () => {
-        removeCartItem(id)
-      }
+      const {cartList, removeAllCartItems} = value
+      const showEmptyView = cartList.length === 0
+      // TODO: Update the functionality to remove all the items in the cart
 
-      const onClickingPlus = () => {
-        incrementCartItemQuantity(id)
+      const onClickingRemoveAll = () => {
+        removeAllCartItems()
       }
-
-      const onClickingMinus = () => {
-        decrementCartItemQuantity(id)
-      }
-      // TODO: Update the functionality to increment and decrement quantity of the cart item
 
       return (
-        <li className="cart-item">
-          <img className="cart-product-image" src={imageUrl} alt={title} />
-          <div className="cart-item-details-container">
-            <div className="cart-product-title-brand-container">
-              <p className="cart-product-title">{title}</p>
-              <p className="cart-product-brand">by {brand}</p>
-            </div>
-            <div className="cart-quantity-container">
-              <button
-                type="button"
-                className="quantity-controller-button"
-                aria-label="quantity"
-                data-testid="minus"
-                onClick={onClickingMinus}
-              >
-                <BsDashSquare color="#52606D" size={12} />
-              </button>
-              <p className="cart-quantity">{quantity}</p>
-              <button
-                type="button"
-                className="quantity-controller-button"
-                aria-label="quantity"
-                data-testid="plus"
-                onClick={onClickingPlus}
-              >
-                <BsPlusSquare color="#52606D" size={12} />
-              </button>
-            </div>
-            <div className="total-price-remove-container">
-              <p className="cart-total-price">Rs {price * quantity}/-</p>
-              <button
-                className="remove-button"
-                type="button"
-                onClick={onRemoveCartItem}
-                data-testid="remove"
-              >
-                Remove
-              </button>
-            </div>
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <button
+                  type="button"
+                  className="remove-all-para"
+                  onClick={onClickingRemoveAll}
+                >
+                  Remove All
+                </button>
+                <CartListView />
+                <CartSummary />
+              </div>
+            )}
           </div>
-          <button
-            className="delete-button"
-            type="button"
-            data-testid="remove"
-            onClick={onRemoveCartItem}
-            aria-label="button"
-          >
-            <AiFillCloseCircle color="#616E7C" size={20} />
-          </button>
-        </li>
+        </>
       )
     }}
   </CartContext.Consumer>
 )
-
-export default CartItem
+export default Cart
